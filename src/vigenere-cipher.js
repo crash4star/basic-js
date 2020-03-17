c621d7b4b10f77937a0cabc6daab91b8d60fdad7
crash4star <47321264+crash4star@users.noreply.github.com>
2020-03-17 15:27:45 +0300

    feat: done vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,65 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(direct = true) {
+        this.direct = direct;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(str, key) {
+        this.validate(str, key);
+
+        let letters = str.toUpperCase().split('');
+        let keyLetters = key.toUpperCase().split('');
+
+        let result = letters.map(letter => {
+            let code = letter.charCodeAt(0) - 65;
+
+            if (code < 0 || code > 25) {
+                return letter;
+            }
+
+            let keyLetter = keyLetters.shift();
+            let keyCode = keyLetter.charCodeAt(0) - 65;
+
+            keyLetters.push(keyLetter);
+
+            return String.fromCharCode((code + keyCode) % 26 + 65);
+        });
+
+
+        return this.prepareResult(result)
+    }
+
+    decrypt(str, key) {
+        this.validate(str, key);
+
+        let letters = str.toUpperCase().split('');
+        let keyLetters = key.toUpperCase().split('');
+
+        let result = letters.map(letter => {
+            let code = letter.charCodeAt(0) - 65;
+
+            if (code < 0 || code > 25) {
+                return letter;
+            }
+
+            let keyLetter = keyLetters.shift();
+            let keyCode = keyLetter.charCodeAt(0) - 65;
+
+            keyLetters.push(keyLetter);
+
+            return String.fromCharCode((code + 26 - keyCode) % 26 + 65);
+        });
+
+        return this.prepareResult(result)
+    }
+
+    validate(str, key) {
+        if (str === undefined || key === undefined) {
+            throw new Error()
+        }
+    }
+
+    prepareResult(result) {
+        return this.direct ? result.join('') : result.reverse().join('');
     }
 }
 
