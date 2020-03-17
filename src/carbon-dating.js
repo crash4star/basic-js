0aa24cc2db3ff4a60007979b9bd06081b4301968
crash4star <47321264+crash4star@users.noreply.github.com>
2020-03-17 15:24:17 +0300

    feat: done carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,24 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const k = 0.693 / HALF_LIFE_PERIOD;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    if (typeof sampleActivity != 'string' || hasLetters(sampleActivity) || invalidRange(sampleActivity)) {
+        return false;
+    }
+
+    const actualActivity = parseFloat(sampleActivity);
+    const age = Math.log(MODERN_ACTIVITY / actualActivity) / k;
+
+    return Math.ceil(age);
 };
+
+function hasLetters(str) {
+    return /[A-Za-z]/.test(str)
+}
+
+function invalidRange(activity) {
+    const float = parseFloat(activity);
+
+    return float <= 0 || float >= MODERN_ACTIVITY;
+}
