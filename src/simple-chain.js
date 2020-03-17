01bb081aaed964f19f96748cd5435b5e12e897da
crash4star <47321264+crash4star@users.noreply.github.com>
2020-03-17 15:26:55 +0300

    feat: done simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,42 @@
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
+
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (value === undefined) {
+      this.chain.push('( )');
+    } else {
+      this.chain.push(`( ${value} )`);
+    }
+    return this;
   },
+
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof position !== 'number' || position <= 0 || !Number.isInteger(position) || position > this.chain.length) {
+      this.chain = [];
+      throw new Error();
+    } else {
+      this.chain.splice(position - 1, 1);
+    }
+
+    return this;
   },
+
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+
+    return this;
   },
+
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let result = this.chain.join('~~');
+
+    this.chain = [];
+
+    return result;
   }
 };
 
